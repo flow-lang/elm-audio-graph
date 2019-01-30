ab63860d6ba17a4f0deb2b35734e837301001ad2
pd-andy <andrew.thompson@qmul.ac.uk>
2019-01-30 18:36:16 +0000

    :sparkles: Generate new version of gh-pages site.

--- docs/__dev/src/Main.elm
+++ docs/__dev/src/Main.elm
@@ -1,14 +1,15 @@
-port module Main exposing (..)
+port module Main exposing (Model, Msg(..), broadcastAudioGraph, codeSnippet_01, init, main, update, view)
 
+import AudioGraph exposing (..)
+import AudioGraph.Encode exposing (encodeGraph)
+import AudioGraph.Units exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Encode as Encode
 
-import AudioGraph exposing (..)
-import AudioGraph.Units exposing (..)
-import AudioGraph.Encode exposing (encodeGraph)
+
 
 -- ---------------------------
 -- MAIN
@@ -29,6 +30,7 @@ main =
         }
 
 
+
 -- ---------------------------
 -- PORTS
 -- ---------------------------
@@ -37,31 +39,37 @@ main =
 port broadcastAudioGraph : Encode.Value -> Cmd msg
 
 
+
 -- ---------------------------
 -- MODEL
 -- ---------------------------
 
 
 type alias Model =
-  { graph : AudioGraph
-  }
+    { graph : AudioGraph
+    }
 
 
 init : () -> ( Model, Cmd Msg )
-init _ = 
-  let 
-    graph =
-      createAudioGraph
-      |> setNode "oscA" createOscillatorNode
-      |> setNode "oscB" (createOscillatorNode
-        |> updateParam "frequency" (Hertz 6))
-      |> setNode "gain" (createGainNode
-        |> updateParam "gain" (Number 0.25))
-      |> addConnection (connect "oscA" 0 "gain" (InputChannel 0))
-      |> addConnection (connect "oscB" 0 "gain" (InputParam "gain"))
-      |> addConnection (connect "gain" 0 "__destination" (InputChannel 0))
-  in
+init _ =
+    let
+        graph =
+            createAudioGraph
+                |> setNode "oscA" createOscillatorNode
+                |> setNode "oscB"
+                    (createOscillatorNode
+                        |> updateParam "frequency" (Hertz 6)
+                    )
+                |> setNode "gain"
+                    (createGainNode
+                        |> updateParam "gain" (Number 0.25)
+                    )
+                |> addConnection (connect "oscA" 0 "gain" (InputChannel 0))
+                |> addConnection (connect "oscB" 0 "gain" (InputParam "gain"))
+                |> addConnection (connect "gain" 0 "__destination" (InputChannel 0))
+    in
     update BroadcastGraph { graph = graph }
+
 
 
 -- ---------------------------
@@ -70,13 +78,15 @@ init _ =
 
 
 type Msg
-   = BroadcastGraph
+    = BroadcastGraph
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    BroadcastGraph ->
-      ( model, broadcastAudioGraph (encodeGraph model.graph) )
+    case msg of
+        BroadcastGraph ->
+            ( model, broadcastAudioGraph (encodeGraph model.graph) )
+
 
 
 -- ---------------------------
@@ -84,6 +94,58 @@ update msg model =
 -- --------------------------
 
 
+codeSnippet_01 : String
+codeSnippet_01 =
+    """import AudioGraph exposing ( createGraph )
+
+graph = createGraph"""
+
+codeSnippet_02 : String
+codeSnippet_02 =
+    """import AudioGraph exposing
+  ( createGraph           -- Creates the audio graph
+  , createOscillatorNode  -- Creates an oscillator node
+  , addNode               -- Adds a node to the graph
+  , addConnection         -- Adds a connection to the graph
+  , connect               -- Creates a connection between two nodes
+  , NodeInput (..)        -- Specifies what type of input to connect to
+  )
+
+graph =
+  createGraph
+    |> addNode \"myOsc\" createOscillatorNode
+    |> addConnection (connect \"myOsc\" 0 \"__destination\" (InputChannel 0))"""
+
+
 view : Model -> Html Msg
 view model =
-  div [] []
+    div [ class "container" ]
+        [ div [ class "row" ]
+            [ div [ class "col" ]
+                [ h1 [ class "title" ]
+                    [ text "elm-audio-graph" ]
+                , p [ class "content" ]
+                    [ text
+                        """
+                        Psst. Here to learn how to make kickass web audio apps with Elm?
+                        Awesome! To get started we need to create an empty audio graph. The
+                        audio graph is where we store all of our audio nodes and keep track
+                        of how they're connected to each other.
+                        """
+                    , pre [] [ text codeSnippet_01 ]
+                    , text
+                        """
+                        Well that wasn't exactly Earth-shattering. To actually make some noise
+                        we need to create some audio nodes and connect them together. Let's start
+                        with a basic sine oscillator that we'll connect to our speakers.
+                        """
+                    , pre [] [ text codeSnippet_02 ]
+                    , text
+                        """
+                        Now we're making some noise but we've just added a lot of
+                        new stuff, what does it all do?
+                        """
+                    ]
+                ]
+            ]
+        ]

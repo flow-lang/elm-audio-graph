92d70c97f13e4b36914549912db56740861da8f2
Andrew Thompson <athom058@gold.ac.uk>
2019-01-30 21:35:06 +0000

    :heavy_plus_sign: Add elm-explorations/markdown to dependencies foor the gh-pages site.

--- docs/__dev/src/Main.elm
+++ docs/__dev/src/Main.elm
@@ -1,4 +1,4 @@
-port module Main exposing (Model, Msg(..), broadcastAudioGraph, codeSnippet_01, init, main, update, view)
+port module Main exposing (..)
 
 import AudioGraph exposing (..)
 import AudioGraph.Encode exposing (encodeGraph)
@@ -8,6 +8,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Encode as Encode
+import Markdown
 
 
 
@@ -94,15 +95,37 @@ update msg model =
 -- --------------------------
 
 
-codeSnippet_01 : String
-codeSnippet_01 =
-    """import AudioGraph exposing ( createGraph )
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ div [ class "row" ]
+            [ div [ class "col" ] [ content ] 
+            ]
+        ]
 
-graph = createGraph"""
 
-codeSnippet_02 : String
-codeSnippet_02 =
-    """import AudioGraph exposing
+content : Html msg
+content =
+    Markdown.toHtml [ class "content" ] """
+# elm-audio-graph
+
+Psst. Here to learn how to make kickass web audio apps with Elm?
+Awesome! To get started we need to create an empty audio graph. The
+audio graph is where we store all of our audio nodes and keep track
+of how they're connected to each other.
+
+```elm
+import AudioGraph exposing ( createGraph )
+
+graph = createGraph
+```
+
+Well that wasn't exactly Earth-shattering. To actually make some noise
+we need to create some audio nodes and connect them together. Let's start
+with a basic sine oscillator that we'll connect to our speakers.
+ 
+```elm
+import AudioGraph exposing
   ( createGraph           -- Creates the audio graph
   , createOscillatorNode  -- Creates an oscillator node
   , addNode               -- Adds a node to the graph
@@ -113,39 +136,23 @@ codeSnippet_02 =
 
 graph =
   createGraph
-    |> addNode \"myOsc\" createOscillatorNode
-    |> addConnection (connect \"myOsc\" 0 \"__destination\" (InputChannel 0))"""
+    |> addNode "myOsc" createOscillatorNode
+    |> addConnection (connect "myOsc" 0 "__destination" (InputChannel 0))
+```
 
+Now we're making some noise but we've just added a lot of
+new stuff, what does it all do?
 
-view : Model -> Html Msg
-view model =
-    div [ class "container" ]
-        [ div [ class "row" ]
-            [ div [ class "col" ]
-                [ h1 [ class "title" ]
-                    [ text "elm-audio-graph" ]
-                , p [ class "content" ]
-                    [ text
-                        """
-                        Psst. Here to learn how to make kickass web audio apps with Elm?
-                        Awesome! To get started we need to create an empty audio graph. The
-                        audio graph is where we store all of our audio nodes and keep track
-                        of how they're connected to each other.
-                        """
-                    , pre [] [ text codeSnippet_01 ]
-                    , text
-                        """
-                        Well that wasn't exactly Earth-shattering. To actually make some noise
-                        we need to create some audio nodes and connect them together. Let's start
-                        with a basic sine oscillator that we'll connect to our speakers.
-                        """
-                    , pre [] [ text codeSnippet_02 ]
-                    , text
-                        """
-                        Now we're making some noise but we've just added a lot of
-                        new stuff, what does it all do?
-                        """
-                    ]
-                ]
-            ]
-        ]
+* First, we create a new audio graph as before.
+* Then, we say we want to add a node to the graph, and give
+  it an id of "myOsc". The id is important because it lets
+  us track changes and updates to a node.
+* We also need to supply the node we want to add. createOscillatorNode
+  returns a new oscilator node with some sensible default values. 
+* Then we add a connection just like we added the node. We need to actually
+  do the connection too. Here we connect our oscillator by supplying the id
+  we created before, "myOsc", and an output channel of 0 and then the id
+  of the node we want to connect to. "__destination" is a special node that
+  gets created for us when we call createGraph and it represents our speakers
+  or some other final output for the graph.
+"""
